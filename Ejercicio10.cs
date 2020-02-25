395137006ed0bea2121d9fea3c87e3de510d42c3
TheDavot <davidzapata5885@gmail.com>
2020-02-24 23:40:29 -0500

    Add files via upload

---
+++ Ejercicio10.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackMulti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine("Blackjack");
+
+            Console.WriteLine("Presiona enter para comenzar a jugar...");
+            Console.ReadLine();
+
+            Console.Write("Ingresa el numero de jugadores (2 - 5): ");
+            int jugador = 0;
+            int jugadorGanador = 0;
+            bool empate = false;
+            int puntajeMayor = 0;
+            int jugadorMayor = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            while (n<2 || n > 5)
+            {
+                Console.Write("Numero invalido de jugadores. Intenta de nuevo: ");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            while (jugador < n)
+            {
+                Console.WriteLine("Turno del jugador " + (jugador + 1));
+                Random aleatorio = new Random();
+                int carta = 0, carta2 = 0;
+                int total = 0;
+                int contador = 0;
+                string continuar = "s";
+                
+                while (continuar == "s" && total < 21)
+                {
+                    carta = aleatorio.Next(1, 11);
+                    //Console.WriteLine("Carta: " + carta);
+
+                    Console.WriteLine("|---------|");
+                    Console.WriteLine("|         |");
+                    Console.WriteLine("|         |");
+                    Console.WriteLine("|    " + carta + "    |");
+                    Console.WriteLine("|         |");
+                    Console.WriteLine("|         |");
+                    Console.WriteLine("|---------|");
+
+                    total += carta;
+
+                    if (contador == 0)
+                    {
+                        carta2 = aleatorio.Next(1, 11);
+                        Console.WriteLine("");
+                        Console.WriteLine("|---------|");
+                        Console.WriteLine("|         |");
+                        Console.WriteLine("|         |");
+                        Console.WriteLine("|    " + carta2 + "    |");
+                        Console.WriteLine("|         |");
+                        Console.WriteLine("|         |");
+                        Console.WriteLine("|---------|");
+                        total += carta2;
+                    }
+
+                    if (total == 21)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("Has ganado");
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("");
+                        if (jugadorGanador == 0)
+                        {
+                            jugadorGanador = jugador + 1;
+                        }
+                        else
+                        {
+                            empate = true;
+                        }
+                        
+                    }
+                    else if (total > 21)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("Has perdido");
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine("");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Total = " + total);
+                        Console.Write("Solicitar carta extra? (s/n)");
+                        continuar = Console.ReadLine();
+
+                        while (continuar != "s" && continuar != "n")
+                        {
+                            Console.Write("Respuesta Incorrecta: ");
+                            continuar = Console.ReadLine();
+                        }   
+                    }
+
+                    contador++;
+                }
+
+            if (jugadorGanador != 0)
+                {
+                    puntajeMayor = 0;
+
+                }else if (total == puntajeMayor){
+                    empate = true;
+                    puntajeMayor = 0;
+                }
+                else if (total > puntajeMayor && total < 21)
+                {
+                    puntajeMayor = total;
+                    jugadorMayor = jugador + 1;
+                }
+            
+            Console.WriteLine("Su total fue: " + total + " puntos");      
+            jugador++;
+            Console.ReadLine();
+            }
+            if (jugadorGanador == 0 && empate == false)
+            {
+                if (puntajeMayor > 0 )
+                {
+                    Console.WriteLine("El jugador que más se acercó es el jugador " + jugadorMayor);
+                }
+                else
+                {
+                    Console.WriteLine("No hay ganador");
+                }               
+            }
+            else if(empate == true)
+            {
+                Console.WriteLine("Empate");
+            }
+            else
+            {
+                Console.WriteLine("El ganador es el jugador " + jugadorGanador);
+            }
+            Console.WriteLine("Gracias por jugar");
+            Console.ReadLine();
+        }
+    }
+}
